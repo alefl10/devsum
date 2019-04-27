6688573aa5f797164709f1feca313671c6e31adf
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-04-27 19:13:37 -0400

    Set up React Router

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import { db } from './config/keys';
 
 const PORT = process.env.PORT || db.PORT;
 
+app.on('listening', () => {
+	console.log(`Server is already listening on port ${PORT}`);
+});
+
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
