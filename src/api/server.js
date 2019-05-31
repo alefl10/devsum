c2584a2b754e92cfe50343e696c5908b99410f50
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-05-30 22:13:30 -0400

    Path fix

--- src/api/server.js
+++ src/api/server.js
@@ -22,11 +22,17 @@ app.use('/api/users', users);
 // Serve static assets if in producion
 if (process.env.NODE_ENV === 'production') {
 	// Static Folder
-	// eslint-disable-next-line no-undef
-	app.use(express.static(path.join(__dirname, '../..', 'client/build')));
+	app.use(express.static('client/build'));
+
 	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, '../..', 'client', 'build', 'index.html'));
+		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
 	});
+
+	// eslint-disable-next-line no-undef
+	// app.use(express.static(path.join(__dirname, '../..', 'client/build')));
+	// app.get('*', (req, res) => {
+	// 	res.sendFile(path.resolve(__dirname, '../..', 'client', 'build', 'index.html'));
+	// });
 }
 
 // Error handler
