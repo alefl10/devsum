4935767fa8f8ea6aca6ad7a831ab26801e879d27
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-05-30 22:52:17 -0400

    Rescructured files

--- src/server.js
+++ src/server.js
@@ -19,7 +19,7 @@ if (process.env.NODE_ENV === 'production') {
 	// Static Folder
 	// eslint-disable-next-line no-undef
 	app.use(express.static(path.join(__dirname, '../', 'client/build')));
-	app.get('*', (req, res) => {
+	app.get('/', (req, res) => {
 		res.sendFile(path.join(__dirname, '../', 'client/build/index.html'));
 	});
 }
