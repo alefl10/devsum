9ca07b6911552832496cff7d49a5011abaf19a5d
Alejandro Ferrero <alejandroferrero@outlook.com>
2019-05-30 22:27:39 -0400

    Changed resolve for join

--- src/api/server.js
+++ src/api/server.js
@@ -22,17 +22,11 @@ app.use('/api/users', users);
 // Serve static assets if in producion
 if (process.env.NODE_ENV === 'production') {
 	// Static Folder
-	app.use(express.static('client/build'));
-
-	app.get('*', (req, res) => {
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-	});
-
 	// eslint-disable-next-line no-undef
-	// app.use(express.static(path.join(__dirname, '../..', 'client/build')));
-	// app.get('*', (req, res) => {
-	// 	res.sendFile(path.resolve(__dirname, '../..', 'client', 'build', 'index.html'));
-	// });
+	app.use(express.static(path.join(__dirname, '../..', 'client/build')));
+	app.get('*', (req, res) => {
+		res.sendFile(path.join(__dirname, '../..', 'client/build/index.html'));
+	});
 }
 
 // Error handler
--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import app from './api/server';
 import { PORT } from './config/keys';
 
-app.on('listening', () => {
-	console.log(`Server is already listening on port ${PORT}`);
-});
-
 app.listen(PORT, () => {
 	console.log(`Server started on port ${PORT}`);
 });
